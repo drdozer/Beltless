aaf662ac26d983088cafaab155b0d8b0bd3a7690
Matthew Pocock <turingatemyhamster@gmail.com>
2020-03-24 17:09:18 +0000

    Initial version 0.1.0 commit.

---
+++ prototypes/item.lua
@@ -0,0 +1,50 @@
+-- Dissable transport belts
+data.raw.item["transport-belt"].place_result = nil
+data.raw["transport-belt"]["transport-belt"].order = data.raw.item["transport-belt"].order
+data.raw["transport-belt"]["transport-belt"].next_upgrade = nil
+data.raw["transport-belt"]["transport-belt"].fast_replaceable_group = nil
+
+data.raw.item["fast-transport-belt"].place_result = nil
+data.raw.item["fast-transport-belt"].next_upgrade = nil
+data.raw["transport-belt"]["fast-transport-belt"].order = data.raw.item["fast-transport-belt"].order
+data.raw["transport-belt"]["fast-transport-belt"].next_upgrade = nil
+data.raw["transport-belt"]["fast-transport-belt"].fast_replaceable_group = nil
+
+data.raw.item["express-transport-belt"].place_result = nil
+data.raw["transport-belt"]["express-transport-belt"].order = data.raw.item["express-transport-belt"].order
+data.raw["transport-belt"]["express-transport-belt"].next_upgrade = nil
+data.raw["transport-belt"]["express-transport-belt"].fast_replaceable_group = nil
+
+-- Dissable underground belts
+data.raw.item["underground-belt"].place_result = nil
+data.raw["underground-belt"]["underground-belt"].order = data.raw.item["underground-belt"].order
+data.raw["underground-belt"]["underground-belt"].next_upgrade = nil
+data.raw["underground-belt"]["underground-belt"].fast_replaceable_group = nil
+
+data.raw.item["fast-underground-belt"].place_result = nil
+data.raw.item["fast-underground-belt"].next_upgrade = nil
+data.raw["underground-belt"]["fast-underground-belt"].order = data.raw.item["fast-underground-belt"].order
+data.raw["underground-belt"]["fast-underground-belt"].next_upgrade = nil
+data.raw["underground-belt"]["fast-underground-belt"].fast_replaceable_group = nil
+
+data.raw.item["express-underground-belt"].place_result = nil
+data.raw["underground-belt"]["express-underground-belt"].order = data.raw.item["express-underground-belt"].order
+data.raw["underground-belt"]["express-underground-belt"].next_upgrade = nil
+data.raw["underground-belt"]["express-underground-belt"].fast_replaceable_group = nil
+
+-- Dissable splitters
+data.raw.item["splitter"].place_result = nil
+data.raw["splitter"]["splitter"].order = data.raw.item["splitter"].order
+data.raw["splitter"]["splitter"].next_upgrade = nil
+data.raw["splitter"]["splitter"].fast_replaceable_group = nil
+
+data.raw.item["fast-splitter"].place_result = nil
+data.raw.item["fast-splitter"].next_upgrade = nil
+data.raw["splitter"]["fast-splitter"].order = data.raw.item["fast-splitter"].order
+data.raw["splitter"]["fast-splitter"].next_upgrade = nil
+data.raw["splitter"]["fast-splitter"].fast_replaceable_group = nil
+
+data.raw.item["express-splitter"].place_result = nil
+data.raw["splitter"]["express-splitter"].order = data.raw.item["express-splitter"].order
+data.raw["splitter"]["express-splitter"].next_upgrade = nil
+data.raw["splitter"]["express-splitter"].fast_replaceable_group = nil
